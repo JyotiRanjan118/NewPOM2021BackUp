dbe5cc101fb1150f6ca3ae16d2bc78a1adaee9e3
JyotiRanjan118 <jyotiranjan118@gmail.com>
2021-09-25 18:27:52 +0530

    Added pages and modified

---
+++ src/main/java/com/qa/hubspot/pages/CartPage.java
@@ -0,0 +1,15 @@
+package com.qa.hubspot.pages;
+
+import org.openqa.selenium.WebDriver;
+
+import com.qa.hubspot.base.BasePage;
+
+public class CartPage extends BasePage {
+	
+	private WebDriver driver;
+	
+	public CartPage(WebDriver driver) {
+		this.driver=driver;
+	}
+	
+}
