c13e215608e8bf45bdc73ecb06d868fe0a165fa4
JyotiRanjan118 <jyotiranjan118@gmail.com>
2021-10-09 16:44:31 +0530

    Browsersupport is added

---
+++ src/main/java/com/qa/hubspot/pages/Browsersupport.java
@@ -0,0 +1,11 @@
+package com.qa.hubspot.pages;
+
+public class Browsersupport {
+	
+	public static void main(String[] args) {
+		System.out.println("chrome");
+		
+		System.out.println("firefox");
+	}
+	
+}	
